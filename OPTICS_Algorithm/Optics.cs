525907df0e58a41b7b30f3e51ea7714cafdd07ba
Barash Serbest <barisserbest127@gmail.com>
2023-05-28 03:10:21 +0300

    OPTICS Algorithm / C#
    Barash Serbest / 5220505051 - C#

---
+++ OPTICS_Algorithm/Optics.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using Accord.Math;
+using Accord.Math.Distances;
+
+namespace OPTICS_Algorithm
+{
+    public class Optics<T>
+    {
+        private readonly int _minPts;
+        private readonly double _xi;
+
+        public Optics(int minPts, double xi)
+        {
+            _minPts = minPts;
+            _xi = xi;
+        }
+
+        public int[] Compute(T[][] data)
+        {
+            int n = data.Length;
+            var visited = new bool[n];
+            var coreDistances = new double[n];
+            var reachabilityDistances = new double[n];
+            var clusterIDs = new int[n];
+            var clusters = new List<int[]>();
+
+            for (int i = 0; i < n; i++)
+            {
+                if (!visited[i])
+                {
+                    visited[i] = true;
+                    var neighbors = RangeSearch(data, data[i]);
+                    if (neighbors.Length < _minPts)
+                    {
+                        coreDistances[i] = double.PositiveInfinity;
+                    }
+                    else
+                    {
+                        var seeds = new List<int>();
+                        Update(data, i, neighbors, visited, coreDistances, seeds);
+                        clusters.Add(seeds.ToArray());
+                    }
+                }
+            }
+
+            int clusterID = 0;
+            for (int i = 0; i < clusters.Count; i++)
+            {
+                var seeds = clusters[i];
+                foreach (int seed in seeds)
+                {
+                    if (reachabilityDistances[seed] <= _xi)
+                    {
+                        clusterIDs[seed] = clusterID;
+                    }
+                    else
+                    {
+                        clusterIDs[seed] = -1;
+                    }
+                }
+                clusterID++;
+            }
+
+            return clusterIDs;
+        }
+
+        private int[] RangeSearch(T[][] data, T[] query)
+        {
+            double[] distances = new double[data.Length];
+            var neighbors = new List<int>();
+            for (int i = 0; i < distances.Length; i++)
+            {
+                if (distances[i] <= _xi)
+                {
+                    neighbors.Add(i);
+                }
+            }
+            return neighbors.ToArray();
+        }
+
+        private void Update(T[][] data, int p, int[] neighbors, bool[] visited, double[] coreDistances, List<int> seeds)
+        {
+            visited[p] = true;
+            seeds.Add(p);
+
+            var coredist = _xi;
+
+            for (int i = 0; i < neighbors.Length; i++)
+            {
+                int q = neighbors[i];
+                if (!visited[q])
+                {
+                    visited[q] = true;
+                    var qNeighbors = RangeSearch(data, data[q]);
+                    if (qNeighbors.Length >= _minPts)
+                    {
+                        Update(data, q, qNeighbors, visited, coreDistances, seeds);
+                    }
+                }
+                if (coreDistances[q] < coredist)
+                {
+                    coredist = coreDistances[q];
+                }
+            }
+
+            coreDistances[p] = coredist;
+        }
+    }
+}
---
+++ OPTICS_Algorithm/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using Accord.MachineLearning;
+using Accord.MachineLearning.VectorMachines;
+using Accord.MachineLearning.VectorMachines.Learning;
+using Accord.Math;
+using Accord.Statistics.Kernels;
+using ExcelDataReader;
+
+/* Bu algoritmayı kullanabilmek için, Accord.NET - Accord.Math - Accord.Statistics.Tools -
+ * Accord.Statistics.Kernels - ExcelDataReader - System.Data kütüphanelerini projenize eklemelisiniz.
+ * Üst kısımdan "Proje" sekmesi içerisinde "Nuget Paketlerini Yönet..." kısmından bu kütüphaneleri ekleyebilirsiniz.    
+*/
+
+namespace OPTICS_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Excel dosyasını okuma
+            DataSet result;
+            using (var stream = File.Open("Mall_Customers.xlsx", FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    result = reader.AsDataSet();
+                }
+            }
+
+            // Verileri alma
+            var table = result.Tables[0];
+            var data = new List<double[]>();
+            for (int i = 0; i < table.Rows.Count; i++)
+            {
+                var row = table.Rows[i];
+                var values = new double[3]
+                {
+                    Convert.ToDouble(row["Age"]),
+                    Convert.ToDouble(row["Annual Income"]),
+                    Convert.ToDouble(row["Spending Score (1-100)"])
+                };
+                data.Add(values);
+            }
+
+            // Verileri ölçeklendirme
+            double[][] scaledData = Accord.Statistics.Tools.ZScores(data.ToArray());
+
+            // OPTICS modelini oluşturma
+            var optics = new Optics<double>(minPts: 5, xi: 0.05);
+
+            // Verilere uygulama
+            var clusterIDs = optics.Compute(scaledData);
+
+            // Sonuçları yazdırma
+            for (int i = 0; i < clusterIDs.Length; i++)
+            {
+                Console.WriteLine($"Veri noktası {i + 1}: Küme ID: {clusterIDs[i]}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
